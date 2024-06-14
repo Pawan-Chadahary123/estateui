14c3a1b66f3af17d49981e0cb2c5763e8fd4ccf7
Pawan Kumar <ts5151822@gmail.com>
2024-06-14 15:33:26 +0530

    use open source map in our project through react leaflet

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,22 +1,26 @@
-import { listData } from "../../lib/dummydata"
-import "./listPage.scss"
-import Filter from "../../components/filter/filter.jsx";
-import Card from "../../components/card/Card.jsx";
 
-export default function ListPage() {
-    const data = listData;
-  return (
-    <div className="listPage">
-     <div className="listContainer">
-        <div className="wrapper">
-            <Filter />
-            {data.map(item =>(
-                <Card key = {item.id} item ={item} />
-            ))}
-        </div>
-     </div>
-     <div className="mapContainer"></div>
-      
+import { listData } from "../../lib/dummydata";
+import "./listPage.scss";
+import Filter from "../../components/filter/filter"
+import Card from "../../components/card/Card"
+import Map from "../../components/map/Map";
+
+function ListPage() {
+  const data = listData;
+
+  return <div className="listPage">
+    <div className="listContainer">
+      <div className="wrapper">
+        <Filter/>
+        {data.map(item=>(
+          <Card key={item.id} item={item}/>
+        ))}
+      </div>
     </div>
-  )
+    <div className="mapContainer">
+      <Map items={data}/>
+    </div>
+  </div>;
 }
+
+export default ListPage;
